dd9fcd69abb29cc0c4325b1852dd166c99ac909c
Thomás Teixeira <78819692+thteixeirap@users.noreply.github.com>
2022-09-19 22:16:10 -0300

    Update hash_and_database.hpp

--- src/hash_and_database.hpp
+++ src/hash_and_database.hpp
@@ -1,19 +1,26 @@
-#ifndef HASH_AND_DATABASE_HPP
-#define HASH_AND_DATABASE_HPP
+#ifndef READDATABASE_HPP
+#define READDATABASE_HPP
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unordered_map>
 #include <vector>
-#include <set>
 #include <string.h>
 #include <fstream>
+#include <queue>
+
 using namespace std;
 
+void readFileD();
+void printHashItens();
+void printHashClasses();
 
-void readFileD(string fileName);
-void readFileT(string fileName);
-void printOutHashT();
-void printOutHashD();
+void readFileT();
+void printFileT();
+void saveCommonItens();
+void printCommonItens();
+void combinations();
+
+void stop();
 
 #endif
