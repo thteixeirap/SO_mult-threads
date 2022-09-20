3f79c85cc4ef06269f6dabad7c2819b6e5783bda
Thomás Teixeira <78819692+thteixeirap@users.noreply.github.com>
2022-09-19 22:16:59 -0300

    Update hash_and_database.cpp

--- src/hash_and_database.cpp
+++ src/hash_and_database.cpp
@@ -1,18 +1,14 @@
-#include "read_database.hpp"
+#include "hash_and_database.hpp"
 
-unordered_map<string, vector<int>> itens;
-unordered_map<string, vector<int>> classes;
-vector<string> commonItens;
-queue<vector<string>> processos;
+unordered_map<string, set<int>> itensD;
+unordered_map<string, set<int>> classesD;
 
-void stop()
-{
-    cout << "\n\nPressione enter para continuar ...";
-    while (getchar() != '\n');
-    cout << endl << endl;
-}
+unordered_map<int, set<string>> itensT;
+vector<string> comparisonResult;
 
-void readFileD()
+
+
+void readFileD(string fileName)
 {
     ifstream file;
     string stringLineOutput;
@@ -21,7 +17,7 @@ void readFileD()
     string key;
     int coluna = 1;
     int linha = 1;
-    file.open("src/dataset/D.csv");
+    file.open(fileName);
 
     if (!file)
     {
@@ -31,154 +27,155 @@ void readFileD()
 
     while (file)
     {
+        if (coluna == 5)
+            coluna = 1;
         getline(file, stringLineOutput);
         charLineOutput = const_cast<char *>(stringLineOutput.c_str());
         token = strtok(charLineOutput, ",");
-        coluna = 1;
+        while (token != NULL)
+        {
+
+            if (coluna == 5)
+            {
+                key = string(token);
+                classesD[key].insert(linha);
+                token = strtok(NULL, ",");
+                break;
+            }
+                key = to_string(coluna) + ", " + string(token);
+                itensD[key].insert(linha);
+                token = strtok(NULL, ",");
+                coluna++;
+            
+        }
+        linha++;
+    }
+
+    file.close();
+    return;
+}
+
+void readFileT(string fileName)
+{
+    ifstream file;
+    string stringLineOutput;
+    char *charLineOutput;
+    char *token;
+    string key;
+    int coluna = 1;
+    int linha = 1;
+    set<string> aux;
+    file.open(fileName);
+
+    if (!file)
+    {
+        printf("file can't be opened \n");
+        return;
+    }
+
+    while (file)
+    {
+        if (coluna == 5)
+            coluna = 1;
+
+        getline(file, stringLineOutput);
+        charLineOutput = const_cast<char *>(stringLineOutput.c_str());
+        token = strtok(charLineOutput, ",");
+
         while (token != NULL)
         {
             if (coluna == 5)
             {
-                key = string(token);
-                classes[key].push_back(linha);
                 break;
             }
+
             key = to_string(coluna) + ", " + string(token);
-            itens[key].push_back(linha);
+            aux.insert(key);
             token = strtok(NULL, ",");
             coluna++;
         }
+
+        itensT[linha].insert(aux.begin(), aux.end());
+        aux.clear();
         linha++;
     }
 
     file.close();
+
     return;
 }
 
-void printHashItens()
+void printOutHashT()
 {
-    cout << "-----------------------ITENS-----------------------" << endl;
-    for (auto mapIt = begin(itens); mapIt != end(itens); ++mapIt)
-    {
-        cout << "(" + mapIt->first + ")"
-             << " : ";
+    cout << endl
+         << "--------- HASH DOS ITENS --------- " << endl
+         << endl;
 
+    for (auto mapIt = begin(itensT); mapIt != end(itensT); ++mapIt)
+    {
         for (auto c : mapIt->second)
-            cout << c << " ";
-
+            cout << '[' + c + ']' << " ";
         cout << endl;
     }
 }
 
-void printHashClasses()
-{
-    cout << "-----------------------CLASSES-----------------------" << endl;
-    for (auto mapIt = begin(classes); mapIt != end(classes); ++mapIt)
-    {
-        cout << "(" + mapIt->first + ")"
-             << " : ";
+void comparingHash(){
 
+    set<string> v;
+   
+    for (auto mapIt = begin(itensT); mapIt != end(itensT); ++mapIt)
+    {
         for (auto c : mapIt->second)
-            cout << c << " ";
-
-        cout << endl;
-    }
-}
-
-void readFileT()
-{
-    ifstream file;
-    string stringLineOutput;
-    char *charLineOutput;
-    char *token;
-    string key;
-    int coluna = 1;
-    int linha = 1;
-    vector<string> v;
-    file.open("src/dataset/T.csv");
-
-    if (!file)
-    {
-        printf("file can't be opened \n");
-        return;
-    }
-
-    while (file)
-    {
-        getline(file, stringLineOutput);
-        charLineOutput = const_cast<char *>(stringLineOutput.c_str());
-        token = strtok(charLineOutput, ",");
-        coluna = 1;
-        while (token != NULL)
         {
-            if (coluna == 5)
-                break;
-            key = to_string(coluna) + ", " + string(token);
-            v.push_back(key);
-            token = strtok(NULL, ",");
-            coluna++;
+            if (itensD.find(c) != itensD.end())
+            {
+               comparisonResult.push_back(c);
+            }
         }
-        processos.push(v);
-        v.clear();
-        linha++;
     }
 
-    file.close();
-    return;
-}
-
-void printFileT()
-{
-    cout << "-----------------------PROCESSOS-----------------------" << endl;
-    queue<vector<string>> processosBackup = processos;
-    vector<string> v;
-    while (!processosBackup.empty())
-    {
-        v = processosBackup.front();
-        processosBackup.pop();
-        for (auto &element : v)
-            cout << "(" << element << ")"
-                 << " ";
-        cout << endl;
-        v.clear();
-    }
-    cout << '\n';
-}
-
-void saveCommonItens()
-{
-    queue<vector<string>> processosBackup = processos;
-    vector<string> v;
-    while (!processosBackup.empty())
-    {
-        v = processosBackup.front();
-        processosBackup.pop();
-        for (auto &element : v)
-        {
-            if(itens.find(element) != itens.end())
-                commonItens.push_back(element);
-        }
-        v.clear();
-    }
-}
-
-void printCommonItens()
-{   
-    cout << "-----------------------VECTOR COM ITENS COMUNS ENTRE ITENS T & D-----------------------" << endl;
-    for(auto &element : commonItens)
-        cout << element << endl;
-}
-
-void combinations()
-{
     ofstream file;
-    file.open("combinations.txt");
-    vector<string> v;
-    for(auto &element : commonItens)
+    file.open("comparisonResult.txt");
+     for(auto &element : comparisonResult)
     {
-        v.push_back(element);
+        v.insert(element);
         for(auto &value : v)
-            file << "(" << value << ")" << " ";
+             file << "(" << value << ")" << " ";
         file << endl;
+    }
+
+
+}
+
+void printOutHashD()
+{
+    cout << endl
+         << "--------- HASH DOS ITENS --------- " << endl
+         << endl;
+
+    for (auto mapIt = begin(itensD); mapIt != end(itensD); ++mapIt)
+    {
+        cout << "  \t[" + mapIt->first + "]"
+             << " : ";
+
+        for (auto c : mapIt->second)
+            cout << c << " ";
+
+        cout << endl;
+    }
+
+    cout << endl
+         << "--------- HASH DAS CLASSES ---------  " << endl
+         << endl;
+
+    for (auto mapIt = begin(classesD); mapIt != end(classesD); ++mapIt)
+    {
+        cout << "[" + mapIt->first + "]"
+             << " : ";
+
+        for (auto c : mapIt->second)
+            cout << c << " ";
+
+        cout << endl;
     }
 }
