1b3ccd73758d51f23db591e3bdd44ad78d447096
Thomás Teixeira <78819692+thteixeirap@users.noreply.github.com>
2022-10-14 20:22:07 -0300

    Update hash_and_database.hpp

--- src/hash_and_database.hpp
+++ src/hash_and_database.hpp
@@ -5,17 +5,24 @@
 #include <stdlib.h>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 #include <queue>
 #include <set>
 #include <string.h>
 #include <fstream>
 using namespace std;
 
-
 void readFileD(string fileName);
 void readFileT(string fileName);
 void printOutHashT();
 void printOutHashD();
-void comparingHash();
+void combinate(vector<string> vector, int perm[], int index, int n, int k);
+void makeCombinate();
+void makeIntersection(string s);
+void intersectionWithClassHash(vector<int> v);
+void intersection();
+void displayResult();
+void printOutHashCombinate();
+vector<int> intersection(vector<int> v1,vector<int> v2);
 
 #endif
