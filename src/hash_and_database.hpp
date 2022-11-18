f71a30eeef609e2da63ba9d3bb044605bf067c16
Thomás Teixeira Pereira <78819692+thteixeirap@users.noreply.github.com>
2022-11-18 17:10:35 -0300

    ultima etapa

--- src/hash_and_database.hpp
+++ src/hash_and_database.hpp
@@ -1,5 +1,6 @@
 #ifndef HASH_AND_DATABASE_HPP
 #define HASH_AND_DATABASE_HPP
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,21 +11,70 @@
 #include <set>
 #include <string.h>
 #include <fstream>
+#include <chrono>
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define NUMCONS 1
+#define NUMPROD 1
+#define BUFFERSIZE 1000
+
 using namespace std;
+using namespace chrono;
+typedef struct classIntersection
+{
+    int versicolor = 0;
+    int virginica = 0;
+    int setosa = 0;
+} classIntersection;
+
+typedef struct classCombination
+{
+    queue<vector<string>> combinates;
+    string auxCombinates;
+    vector<string> allCombinatesLine;
+    vector<int> nulo = {-1};
+} classCombination;
+
+typedef struct unordered_global
+{
+    unordered_map<string, vector<int>> itensD;
+    unordered_map<string, vector<int>> classesD;
+    unordered_map<string, vector<int>> cache_intersection;
+    unordered_map<string, vector<int>> intersectionWithClasse;
+
+} unordered_global;
+
+typedef struct
+{
+    int buffer[BUFFERSIZE];
+    string buffer2[BUFFERSIZE];
+    int currentidx;
+    bool finishedProd = false;
+    pthread_mutex_t buffer_mutex;
+    sem_t buffer_full;
+    sem_t buffer_empty;
+} estrutura_global;
 
 void readFileD(string fileName);
 void readFileT(string fileName);
-void printOutHashT();
-void printOutHashD();
 void combinate(vector<string> vector, int perm[], int index, int n, int k);
 void makeCombinate();
 void makeIntersection(string keyInString);
-void intersectionWithClassHash(vector<int> v);
+void intersectionWithClassHash(string Key, vector<int> v);
 void intersection();
-void displayResult();
-void printOutHashCombinate();
 unordered_map<string, vector<int>>::const_iterator cacheSearch(string key);
-vector<int> intersection(vector<int> v1,vector<int> v2);
-void returnsWinningClass();
+vector<int> intersection(vector<int> v1, vector<int> v2);
 string tokenizeString(string s);
+void returnsWinningClass();
+void displayResult();
+// ===========================================
+void *consumidor(void *arg);
+void *produtor(void *arg);
+void printOutHashCombinate();
+void printOutHashD();
+
+
 #endif
